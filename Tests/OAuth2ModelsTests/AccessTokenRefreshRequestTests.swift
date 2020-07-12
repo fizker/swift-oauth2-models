e21a9f0b15dfb118caeadaaded85665c9a75ad22
Benjamin Horsleben <benjamin@fizkerinc.dk>
2020-07-13 01:50:03 +0200

    Added tests regarding encoding some of the data types

--- Tests/OAuth2ModelsTests/AccessTokenRefreshRequestTests.swift
+++ Tests/OAuth2ModelsTests/AccessTokenRefreshRequestTests.swift
@@ -13,44 +13,63 @@ final class AccessTokenRefreshRequestTests: XCTestCase {
 	}
 
 	func test__setUp__noChanges__decodesCorrectly() throws {
-		let data = try encodeData()
-		XCTAssertNoThrow(try decode(data))
+		let data = try encode(json)
+		XCTAssertNoThrow(try decode(data) as AccessTokenRefreshRequest)
 	}
 
 	func test__decodable__missingScope__returnsDecodedObject() throws {
 		json.removeValue(forKey: .scope)
-		let data = try encodeData()
-		let request = try decode(data)
+		let data = try encode(json)
+		let request = try decode(data) as AccessTokenRefreshRequest
 
 		XCTAssertNil(request.scope)
 	}
 
 	func test__decodable__invalidGrantType__throws() throws {
 		json[.grantType] = "foo"
-		let data = try encodeData()
+		let data = try encode(json)
 
-		XCTAssertThrowsError(try decode(data))
+		XCTAssertThrowsError(try decode(data) as AccessTokenRefreshRequest)
 	}
 
 	func test__decodable__missingGrantType__throws() throws {
 		json.removeValue(forKey: .grantType)
-		let data = try encodeData()
+		let data = try encode(json)
 
-		XCTAssertThrowsError(try decode(data))
+		XCTAssertThrowsError(try decode(data) as AccessTokenRefreshRequest)
 	}
 
 	func test__decodable__missingRefreshToken__throws() throws {
 		json.removeValue(forKey: .refreshToken)
-		let data = try encodeData()
+		let data = try encode(json)
 
-		XCTAssertThrowsError(try decode(data))
+		XCTAssertThrowsError(try decode(data) as AccessTokenRefreshRequest)
 	}
 
-	private func decode(_ data: Data) throws -> AccessTokenRefreshRequest {
-		return try OAuth2ModelsTests.decode(data)
+	func test__encodable__fullObject__encodesAsExpected() throws {
+		let object = AccessTokenRefreshRequest(refreshToken: "foo", scope: "bar")
+		let data = try encode(object)
+		let actual = try decode(data) as [String: String]
+
+		let expected = [
+			"grant_type": "refresh_token",
+			"refresh_token": "foo",
+			"scope": "bar",
+		]
+
+		XCTAssertEqual(actual, expected)
 	}
 
-	private func encodeData() throws -> Data {
-		return try encode(json)
+	func test__encodable__minimalObject__encodesAsExpected() throws {
+		let object = AccessTokenRefreshRequest(refreshToken: "foo")
+		let data = try encode(object)
+		let actual = try decode(data) as [String: String]
+
+		let expected = [
+			"grant_type": "refresh_token",
+			"refresh_token": "foo",
+		]
+
+		XCTAssertEqual(actual, expected)
 	}
 }
--- Tests/OAuth2ModelsTests/AccessTokenRequestTests.swift
+++ Tests/OAuth2ModelsTests/AccessTokenRequestTests.swift
@@ -1,6 +1,12 @@
 import XCTest
 import OAuth2Models
 
+extension URL: ExpressibleByStringLiteral {
+	public init(stringLiteral value: StringLiteralType) {
+		self.init(string: value)!
+	}
+}
+
 final class AccessTokenRequestTests: XCTestCase {
 	var json: [AccessTokenRequest.CodingKeys: String] = [:]
 
@@ -14,23 +20,23 @@ final class AccessTokenRequestTests: XCTestCase {
 	}
 
 	func test__setUp__noChanges__decodesCorrectly() throws {
-		let data = try encodeData()
-		XCTAssertNoThrow(try decode(data))
+		let data = try encode(json)
+		XCTAssertNoThrow(try decode(data) as AccessTokenRequest)
 	}
 
 	func test__decodable__missingRedirectURI__returnsDecodedObject() throws {
 		json.removeValue(forKey: .redirectURL)
-		let data = try encodeData()
-		let request = try decode(data)
+		let data = try encode(json)
+		let request = try decode(data) as AccessTokenRequest
 
 		XCTAssertNil(request.redirectURL)
 	}
 
 	func test__decodable__invalidGrantType__throws() throws {
 		json[.grantType] = "foo"
-		let data = try encodeData()
+		let data = try encode(json)
 
-		XCTAssertThrowsError(try decode(data))
+		XCTAssertThrowsError(try decode(data) as AccessTokenRequest)
 	}
 
 	func test__decodable__invalidRedirectURL__throws() throws {
@@ -39,14 +45,43 @@ final class AccessTokenRequestTests: XCTestCase {
 		json[.redirectURL] = "invalid"
 		let data = try encode(json)
 
-		XCTAssertThrowsError(try decode(data))
+		XCTAssertThrowsError(try decode(data) as AccessTokenRequest)
 	}
 
-	private func decode(_ data: Data) throws -> AccessTokenRequest {
-		return try OAuth2ModelsTests.decode(data)
+	func test__encodable__fullObject__encodesAsExpected() throws {
+		let object = AccessTokenRequest(
+			grantType: .authorizationCode,
+			code: "foo",
+			redirectURL: "https://example.com",
+			clientID: "baz"
+		)
+		let data = try encode(object)
+		let actual = try decode(data) as [String: String]
+
+		let expected = [
+			"grant_type": "authorization_code",
+			"code": "foo",
+			"redirect_uri": "https://example.com",
+			"client_id": "baz",
+		]
+
+		XCTAssertEqual(actual, expected)
 	}
 
-	private func encodeData() throws -> Data {
-		return try encode(json)
+	func test__encodable__minimalObject__encodesAsExpected() throws {
+		let object = AccessTokenRequest(
+			code: "foo",
+			redirectURL: nil,
+			clientID: nil
+		)
+		let data = try encode(object)
+		let actual = try decode(data) as [String: String]
+
+		let expected = [
+			"grant_type": "authorization_code",
+			"code": "foo",
+		]
+
+		XCTAssertEqual(actual, expected)
 	}
 }
